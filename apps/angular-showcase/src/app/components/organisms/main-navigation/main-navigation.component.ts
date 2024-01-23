d6cafa93ee563bd276a4e52d38a525345abb8157
p-parafiniuk <pawel.parafiniuk@netguru.com>
2024-01-23 13:50:19 +0100

    Add routerLink for internal test in main-navigation component

--- apps/angular-showcase/src/app/components/organisms/main-navigation/main-navigation.component.ts
+++ apps/angular-showcase/src/app/components/organisms/main-navigation/main-navigation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'nx-playground-main-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './main-navigation.component.html',
   styleUrl: './main-navigation.component.scss',
 })
