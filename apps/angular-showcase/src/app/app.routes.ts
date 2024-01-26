f6d46f26e9e7ab998d4ea6e5e5edc4c5263363cc
p-parafiniuk <pawel.parafiniuk@netguru.com>
2024-01-26 12:28:11 +0100

    Add page404 component to handle 404 errors

--- apps/angular-showcase/src/app/app.routes.ts
+++ apps/angular-showcase/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Route } from '@angular/router';
 import { InternalTestComponent } from './components/Pages/InternalTest/internal-test.component';
 import { MainPageComponent } from './components/Pages/MainPage/main-page.component';
+import { Page404Component } from './components/Pages/page404/page404.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: MainPageComponent, title: 'Main page' },
+  // { path: 'login', component: MainPageComponent, title: 'Login page' },
   {
     path: 'internal-test',
     component: InternalTestComponent,
     title: 'Internal test page',
   },
+  { path: '**', pathMatch: 'full', component: Page404Component, title: '404' },
 ];
---
+++ apps/angular-showcase/src/app/components/Pages/page404/page404.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-playground-page404',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page404.component.html',
+  styleUrl: './page404.component.scss',
+})
+export class Page404Component {}
