0e2471e9c979ba792446d62d343284673f17d867
p-parafiniuk <pawel.parafiniuk@netguru.com>
2024-01-26 13:05:43 +0100

    Add debug information to page404 component

--- apps/angular-showcase/src/app/components/Pages/page404/page404.component.ts
+++ apps/angular-showcase/src/app/components/Pages/page404/page404.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'nx-playground-page404',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './page404.component.html',
   styleUrl: './page404.component.scss',
 })
-export class Page404Component {}
+export class Page404Component {
+  constructor(public activatedRoute: ActivatedRoute) {
+    console.log('Page404Component', activatedRoute);
+  }
+}
